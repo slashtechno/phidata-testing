1613e96a82db949e10dd1668920dc76820771960
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-12 20:29:27 -0400

    Attempt to use RAG

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,11 @@
 from phi.assistant import Assistant
-
-# from phi.tools.website import WebsiteTools
 from phi.llm.ollama import Ollama
 import ollama
-
-# from phi.knowledge.website import WebsiteKnowledgeBase
-# from phi.knowledge.combined import CombinedKnowledgeBase
 from phi.vectordb.pgvector import PgVector2
 from phi.embedder.ollama import OllamaEmbedder
-from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.knowledge.pdf import PDFKnowledgeBase, PDFReader
 
-
-model = "llama2-uncensored"
-# model = "llama3.1:8b"
+model = "llama3.1:8b"
 # model = "adrienbrault/nous-hermes2theta-llama3-8b:q8_0"
 # model = "nexusraven"
 host = None
@@ -32,26 +25,29 @@ llm = Ollama(model=model)
 # ),
 # )
 
-knowledge_base = PDFUrlKnowledgeBase(
-    urls=["https://www.accessdata.fda.gov/drugsatfda_docs/nda/2018/210951Orig1s000MultidisciplineR.pdf", "https://www.accessdata.fda.gov/drugsatfda_docs/nda/2018/rev_210951_arn-509-003_PRO_Redacted.pdf"],
+knowledge_base = PDFKnowledgeBase(
+    path="data",
     vector_db=PgVector2(
         collection="pdf_documents",
         db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
         embedder=OllamaEmbedder(
-            model=model,
+            model="nomic-embed-text:latest",
+            dimensions=768,
         ),
     ),
+    reader=PDFReader(chunk=True)
 )
 
 
 assistant = Assistant(
     llm=llm,
     debug_mode=True,
-    prevent_hallucinations=False,
     knowledge_base=knowledge_base,
+    add_references_to_prompt=True,
 )
+
+
 # assistant.knowledge_base.load(recreate=False)
-assistant.knowledge_base.load()
-assistant.cli_app()
-# assistant.print_response("Who was the clinical team leader in the multi-discipline review of erleada by the FDA?", markdown=False)
-print("Done!")
+assistant.knowledge_base.load(recreate=True, upsert=True)
+# assistant.cli_app()
+assistant.print_response("Who was the clinical team leader", markdown=False)
